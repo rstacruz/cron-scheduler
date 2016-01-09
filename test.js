ea760fa9d78a514c3183bfa227309806acb37f31
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-10 00:18:39 +0800

    Improve tests

--- test.js
+++ test.js
@@ -1,10 +1,50 @@
 var test = require('tape')
-var cron = require('./index')
-var moment = require('moment-timezone')
-cron.debug(console.log.bind(console))
+var sandbox = require('sinon-in-sandbox')
 
 test('cron', function (t) {
-  var job = cron({ on: '0 9 * * *' }, function () {})
-  job.stop()
-  t.end()
+  sandbox(function (sinon, clock) {
+    var clock = sinon.useFakeTimers()
+    t.ok(+new Date() === 0, 'sinon timers is working')
+
+    var cron = rerequire('./index')
+
+    var job = cron({ on: '0 1 * * *', timezone: 'GMT' }, function () {
+      t.pass('called')
+      job.stop()
+      t.end()
+    })
+
+    clock.tick(3600 * 1000)
+  })
 })
+
+test('cron timezones', function (t) {
+  sandbox(function (sinon) {
+    var clock = sinon.useFakeTimers()
+    var cron = rerequire('./index')
+
+    // Asia/Manila is +0800 GMT, so this is 1AM GMT
+    var job = cron({ on: '0 9 * * *', timezone: 'Asia/Manila' }, function () {
+      t.pass('called')
+      job.stop()
+      t.end()
+    })
+
+    clock.tick(3600 * 1000)
+  })
+})
+
+/*
+ * substitute for require() that will not cache its result, so it make be
+ * re-required again in the future. we need this so that cron/moment will
+ * pick up the sandboxed Date object.
+ */
+
+function rerequire (mod) {
+  var keys = Object.keys(require.cache)
+  var result = require(mod)
+  Object.keys(require.cache).forEach(function (key) {
+    if (keys.indexOf(key) === -1) delete require.cache[key]
+  })
+  return result
+}
